532f0fcd6d47af5992301cda503ddbae81a357ec
aliemir.frnc <77262454+aliemirfrnc@users.noreply.github.com>
2022-01-13 21:43:18 +0300

    Update SınavSorusu.cs

--- SınavSorusu.cs
+++ SınavSorusu.cs
@@ -3,37 +3,51 @@ using System.Collections.Generic;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         try
         {
-            List<Ogrenci> ogrenciliste = new List<Ogrenci>();
-            ogrenciliste.Add(new OrtaOkulOgrencisi("Zehra", "ACI", 354, 1990, 2019));
-            ogrenciliste.Add(new IlkOkulOgrencisi("Esra", "ZEYBEK", 985, 2014, 2000));
-            ogrenciliste.Add(new OrtaOkulOgrencisi("Ali", "AKIN", 654, 2010, 2020));
-            ogrenciliste.Add(new IlkOkulOgrencisi("Ahmet", "EKER", 387, 2015, 2000));
-            ogrenciliste.Add(new OrtaOkulOgrencisi("Ahmet", "ÇELİK", 658, 2011, 2022));
-            ogrenciliste.Add(new IlkOkulOgrencisi("Arda", "AYDIN", 498, 2013, 2008));
-            foreach (var herbirogrenci in ogrenciliste)
+        List<Ogrenci> ogrenciliste = new List<Ogrenci>();
+        ogrenciliste.Add(new OrtaOkulOgrencisi("Zehra", "ACI", 354, 2009, 2019));
+        ogrenciliste.Add(new IlkOkulOgrencisi("Esra", "ZEYBEK", 985, 2014, 2000));
+        ogrenciliste.Add(new OrtaOkulOgrencisi("Ali", "AKIN", 654, 2010, 2020));
+        ogrenciliste.Add(new IlkOkulOgrencisi("Ahmet", "EKER", 387, 2015, 2000));
+        ogrenciliste.Add(new OrtaOkulOgrencisi("Ahmet", "ÇELİK", 658, 2011, 2022));
+        ogrenciliste.Add(new IlkOkulOgrencisi("Arda", "AYDIN", 498, 2013, 2008));
+        foreach (var ogrenciler in ogrenciliste)
+        {
+            ogrenciler.bilgileriEkranaYaz();
+            if (ogrenciler is IlkOkulOgrencisi)
             {
-                herbirogrenci.BilgileriEkranaYaz();
-                if (herbirogrenci is IlkOkulOgrencisi)
-                {
-                    ((IlkOkulOgrencisi)herbirogrenci).ortaokuldanMezunOlmaYili();
-                }
-                else if (herbirogrenci is OrtaOkulOgrencisi)
-                {
-                    ((OrtaOkulOgrencisi)herbirogrenci).gidebileceğiOkullar();
-                }
+                ((IlkOkulOgrencisi)ogrenciler).ortaokuldanMezunOlmaYılı();
+            }
+            else if(ogrenciler is OrtaOkulOgrencisi)
+            {
+                ((OrtaOkulOgrencisi)ogrenciler).GidebilecegiOkullar();
             }
         }
-        catch (Exception HATA)
+        Console.WriteLine("\n");
+        Console.WriteLine("/////Silme İşleminden Sonra/////");    
+        ogrenciliste.RemoveAt(3);
+        foreach (var silindiktensonra in ogrenciliste)
         {
-            Console.WriteLine($"[HATA]:{HATA.Message}");
+            silindiktensonra.bilgileriEkranaYaz();
+            if (silindiktensonra is IlkOkulOgrencisi)
+            {
+                ((IlkOkulOgrencisi)silindiktensonra).ortaokuldanMezunOlmaYılı();
+            }
+            else if (silindiktensonra is OrtaOkulOgrencisi)
+            {
+                ((OrtaOkulOgrencisi)silindiktensonra).GidebilecegiOkullar();
+            }
         }
 
+        }
+        catch (Exception Hata)
+        {
 
-        
+            Console.WriteLine($"[Hata]:{Hata.Message}");
+        }
     }
 }
 abstract class Ogrenci
@@ -51,15 +65,17 @@ abstract class Ogrenci
         }
         set
         {
-            if (value.Trim().Length == 0)
+            if (value.Trim()=="")
             {
-                throw new Exception("Ad Değeri boş olamaz");
+                throw new Exception("Ad Boş Geçilmez");
             }
             else
             {
                 ad = value;
             }
+                      
         }
+
     }
     public string Soyad
     {
@@ -69,9 +85,9 @@ abstract class Ogrenci
         }
         set
         {
-            if (value.Trim().Length == 0)
+            if (value.Trim()=="")
             {
-                throw new Exception("Soyad Değeri boş olamaz");
+                throw new Exception("SoyAd Boş Geçilmez");
             }
             else
             {
@@ -87,13 +103,13 @@ abstract class Ogrenci
         }
         set
         {
-            if (value >= 100 && value <= 999)
+            if (value >=100 && value <=999)
             {
                 ogrenciNo = value;
             }
             else
             {
-                throw new Exception("Ogrenci No 3 Basamaklı olmalı!!");
+                throw new Exception("3 Basamaklı Olmalı");
             }
         }
     }
@@ -105,13 +121,13 @@ abstract class Ogrenci
         }
         set
         {
-            if (value >= 1990 && value <= DateTime.Now.Year)
+            if (value < DateTime.Now.Year && value > 1990)
             {
                 dogumTarihi = value;
             }
             else
             {
-                throw new Exception("Dogum Tarihi 1990'dan büyük olmalı");
+                throw new Exception("Doğum Tarihi Hatalı");
             }
         }
     }
@@ -123,13 +139,13 @@ abstract class Ogrenci
         }
         set
         {
-            if (value >= 1990 && value <= DateTime.Now.Year)
+            if (value < DateTime.Now.Year && value > 1990)
             {
                 baslamaYili = value;
             }
             else
             {
-                throw new Exception("Başlama Yılı 1990'dan büyük olmalı");
+                throw new Exception("Başlama Yılı Hatalı");
             }
         }
     }
@@ -140,7 +156,7 @@ abstract class Ogrenci
             return DateTime.Now.Year - DogumTarihi;
         }
     }
-    public Ogrenci(string adP,string soyadP,int ogrenciNoP,int dogumTarihiP,int baslamaYiliP)
+    public Ogrenci(string adP, string soyadP, int ogrenciNoP, int dogumTarihiP, int baslamaYiliP)
     {
         this.Ad = adP;
         this.Soyad = soyadP;
@@ -149,95 +165,86 @@ abstract class Ogrenci
         this.BaslamaYili = baslamaYiliP;
     }
     public abstract string enOnemliOlay();
-    public virtual void BilgileriEkranaYaz()
+    public virtual void bilgileriEkranaYaz()
     {
-        Console.WriteLine("-----");
-        Console.WriteLine($"Ad:{Ad}");
-        Console.WriteLine($"Soyad:{Soyad}");
+        Console.WriteLine("/////////////////////");
+        Console.WriteLine($"Ad Soyad:{Ad} {Soyad}");
         Console.WriteLine($"Öğrenci No:{OgrenciNo}");
-        Console.WriteLine($"Doğum Tarihi/Yaş:{DogumTarihi}-{Yas}");
-        Console.WriteLine($"Başlama Yılı:{BaslamaYili}");
-        Console.WriteLine("-----");
-
-
-    }
+        Console.WriteLine($"Doğum Tarihi:{DogumTarihi}/{Yas}");
+        Console.WriteLine($"Başlama Yılı:{BaslamaYili}");        
+    }    
 }
-interface IOgrencİslem
+interface IOgrenciİslem
 {
-    string matematikDersİcerigi();
-    int mezunOlacagiYas();
+    string MatematikDersİcerigi();
+    int MezunOlacagiYas();
 }
-class IlkOkulOgrencisi : Ogrenci, IOgrencİslem
+class IlkOkulOgrencisi : Ogrenci, IOgrenciİslem
 {
     public IlkOkulOgrencisi(string adP, string soyadP, int ogrenciNoP, int dogumTarihiP, int baslamaYiliP) : base(adP, soyadP, ogrenciNoP, dogumTarihiP, baslamaYiliP)
     {
-        Console.WriteLine("İlk Okul Öğrenci Nesnesi Oluşturuldu");
+        Console.WriteLine("İlkOkul Öğrencisi Oluşturuldu");
     }
 
     public override string enOnemliOlay()
     {
-        return "Yaz Kampları";
+        return "Yaz Kampı";
     }
 
-    public string matematikDersİcerigi()
+    public string MatematikDersİcerigi()
     {
         return "İlk Matematik";
     }
 
-    public int mezunOlacagiYas()
+    public int MezunOlacagiYas()
     {
         return Yas + 4;
+    }
+    public override void bilgileriEkranaYaz()
+    {
+        base.bilgileriEkranaYaz();
+        Console.WriteLine($"En Önemli Olayı:{enOnemliOlay()}");
+        Console.WriteLine($"Matematik Ders İçeriği:{MatematikDersİcerigi()}");
+        Console.WriteLine($"Mezun Olacağı Yaş:{MezunOlacagiYas()}");
     }
     public override string ToString()
     {
         return $"{Ad} {Soyad}";
     }
-    public override void BilgileriEkranaYaz()
+    public void ortaokuldanMezunOlmaYılı()
     {
-        base.BilgileriEkranaYaz();
-        Console.WriteLine($"En Önemli Olay:{enOnemliOlay()}");
-        Console.WriteLine($"Matematik Ders İçeriği:{matematikDersİcerigi()}");
-        Console.WriteLine($"Mezun Olacağı Yaş:{mezunOlacagiYas()}");
-    }
-    public void ortaokuldanMezunOlmaYili()
-    {
-        Console.WriteLine($"OrtaOkuldan Mezun Olacagi Yıl:{BaslamaYili+8}");
+        Console.WriteLine($"OrtaOkuldan Mezun Olma Yılı:{BaslamaYili + 8}");
     }
 }
-class OrtaOkulOgrencisi : Ogrenci, IOgrencİslem
-{
+class OrtaOkulOgrencisi : Ogrenci, IOgrenciİslem
+{  
     public OrtaOkulOgrencisi(string adP, string soyadP, int ogrenciNoP, int dogumTarihiP, int baslamaYiliP) : base(adP, soyadP, ogrenciNoP, dogumTarihiP, baslamaYiliP)
     {
         Console.WriteLine("OrtaOkul Öğrenci Nesnesi Oluşturuldu");
     }
-
     public override string enOnemliOlay()
     {
         return "LGS Sınavı";
     }
 
-    public string matematikDersİcerigi()
+    public string MatematikDersİcerigi()
     {
         return "Temel Matematik";
     }
 
-    public int mezunOlacagiYas()
+    public int MezunOlacagiYas()
     {
-        return Yas + 8;
+        return Yas + 4;
     }
-    public override string ToString()
-    {
-        return $"{Ad} {Soyad} {OgrenciNo}";
-    }
-    public override void BilgileriEkranaYaz()
-    {
-        base.BilgileriEkranaYaz();
-        Console.WriteLine($"En Önemli Olay:{enOnemliOlay()}");
-        Console.WriteLine($"Matematik Ders İçeriği:{matematikDersİcerigi()}");
-        Console.WriteLine($"Mezun Olacağı Yaş:{mezunOlacagiYas()}");
-    }
-    public void gidebileceğiOkullar()
-    {
-        Console.WriteLine("Gidebileceği Okullar: MTAL, Anadolu Lisesi, Fen Lisesi");
-    }
+    public override void bilgileriEkranaYaz()
+        {
+            base.bilgileriEkranaYaz();
+            Console.WriteLine($"En Önemli Olayı:{enOnemliOlay()}");
+            Console.WriteLine($"Matematik Ders İçeriği:{MatematikDersİcerigi()}");
+            Console.WriteLine($"Mezun Olacağı Yaş:{MezunOlacagiYas()}");
+        }
+    public void GidebilecegiOkullar()
+        {
+            Console.WriteLine("Gidebileceği Okullar: MTAL, Anadolu Lisesi, Fen Lisesi");
+        }
 }
